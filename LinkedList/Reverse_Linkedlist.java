a66083e247f103a12783e3b240b31efff372274d
RADHIKAagrawal2207 <radhikaagarwal227@gmail.com>
2025-07-03 22:25:49 +0530

    add

---
+++ LinkedList/Reverse_Linkedlist.java
@@ -0,0 +1,20 @@
+package JAVA.LinkedList;
+//import java.util.*;
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+public class Reverse_Linkedlist {
+    
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode rev=reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return rev;
+    }
+}
