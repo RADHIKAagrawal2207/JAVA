9434e22f09912c6b5eb4faae7b6a4cd926aaa5f8
RADHIKAagrawal2207 <radhikaagarwal227@gmail.com>
2025-07-05 10:59:28 +0530

    add

---
+++ Array/RemoveDuplicate.java
@@ -0,0 +1,22 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicate {
+    class Solution {
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> set = new LinkedHashSet<>();
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int i = 0;
+        for (int num : set) {
+            nums[i++] = num;
+        }
+
+        return set.size();
+    }
+}
+
+}
