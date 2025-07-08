349dff1c7edf1404769d0e54ce274549ee60f075
RADHIKAagrawal2207 <radhikaagarwal227@gmail.com>
2025-07-08 21:03:31 +0530

    add

---
+++ Recursion/PredictWinner.java
@@ -0,0 +1,14 @@
+package JAVA.Recursion;
+public class PredictWinner {
+    class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        return sum(nums,0,nums.length-1)>=0;
+    }
+    public int sum(int[] nums,int i,int j){
+        if(i==j) return nums[j];
+        int s1=nums[i]-sum(nums,i+1,j);
+        int s2=nums[j]-sum(nums,i,j-1);
+        return Math.max(s1,s2);
+    }
+}
+}
