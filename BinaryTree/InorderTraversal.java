4d30f4401b13da9c10b0c3e9e1dcbe7038df3282
RADHIKAagrawal2207 <radhikaagarwal227@gmail.com>
2025-07-09 23:56:28 +0530

    add

---
+++ BinaryTree/InorderTraversal.java
@@ -0,0 +1,39 @@
+package JAVA.BinaryTree;
+import java.util.*;
+public class InorderTraversal {
+    
+
+
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorderTraversal(root, list);
+        return list;
+    }
+
+    void inorderTraversal(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+
+        inorderTraversal(node.left, list);
+        list.add(node.val); 
+        inorderTraversal(node.right, list); 
+    }
+}
+}
+}
+
